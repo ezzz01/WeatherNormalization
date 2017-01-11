3f3f58beb2522bce4997eea1fc287040b367690a
tadas.sugintas <tadas.sugintas@maalka.com>
2017-01-11 10:22:02 +0200

    Trying with K=2 first, if that fails, trying with K=1

--- src/main/scala/com/maalka/WeatherNormalization.scala
+++ src/main/scala/com/maalka/WeatherNormalization.scala
@@ -23,40 +23,68 @@ object WeatherNormalization {
 
           segs <- segmented(fit, seg.Z = ~temperature, psi=NA, seg.control(K=K_input))
           coefs = coef(segs)
-          result <- list(coefficients = coefs, psi = segs['psi'], residuals = segs['residuals'])
+          result <- list(coefficients = coefs, psi = segs[['psi']], residuals = segs['residuals'])
         }
       """
 
     R.set ("temperature", temperature)
     R.set ("energy", energy)
 
-    try {
+    var result: Option[SegmentedRegressionResult] = None
 
       if (temperature.length > 11) {
-        R.set("K_input", 1)
-        // run once more with 1 if 2 fails
-        // R.set("K_input", 2)
+
+        try {
+          Console.println("Segmented regression with K=2")
+          R.set("K_input", 2)
+
+          val ref = R.evalR(s"regress()")
+          val breakpoints = R.evalD1(s"unlist(${ref}['psi'])")
+          val coefficients = R.evalD1(s"unlist(${ref}[['coefficients']])")
+          val residuals = R.evalD1(s"unlist(${ref}['residuals'])")
+
+          val break1 =  BreakPoint(breakpoints(0), breakpoints(2), breakpoints(4))
+          val break2 =  BreakPoint(breakpoints(1), breakpoints(3), breakpoints(5))
+
+          result = Some(SegmentedRegressionResult(List(break1, break2), residuals, coefficients))
+
+        } catch {
+          case re: RuntimeException => {
+            Console.println("Segmented regression with K=1")
+            try {
+              R.set("K_input", 1)
+
+              val ref = R.evalR(s"regress()")
+              val breakpoints = R.evalD1(s"unlist(${ref}['psi'])")
+              val coefficients = R.evalD1(s"unlist(${ref}[['coefficients']])")
+              val residuals = R.evalD1(s"unlist(${ref}['residuals'])")
+
+              val break1 = BreakPoint(breakpoints(0), breakpoints(1), breakpoints(2))
+              result = Some(SegmentedRegressionResult(List(break1), residuals, coefficients))
+            } catch {
+              case re: RuntimeException => None
+            }
+          }
+        }
+
       } else if (temperature.length > 6 && temperature.length <= 11) {
-        R.set("K_input", 1)
+        try {
+          R.set("K_input", 1)
+          val ref = R.evalR(s"regress()")
+          val breakpoints = R.evalD1(s"unlist(${ref}['psi'])")
+          val coefficients = R.evalD1(s"unlist(${ref}[['coefficients']])")
+          val residuals = R.evalD1(s"unlist(${ref}['residuals'])")
+
+          val break1 = BreakPoint(breakpoints(0), breakpoints(1), breakpoints(2))
+          result = Some(SegmentedRegressionResult(List(break1), residuals, coefficients))
+        } catch {
+          case re: RuntimeException => None
+        }
+
       }
 
-      val ref:RObject = R.evalR(s"regress()")
+      result
 
-      val breakpoints = R.evalD1(s"unlist(${ref}['psi'])")
-      val coefficients = R.evalD1(s"unlist(${ref}['coefficients'])")
-      val residuals = R.evalD1(s"unlist(${ref}['residuals'])")
-
-      val break1 =  BreakPoint(breakpoints(0), breakpoints(1), breakpoints(2))
-
-      // not sure what indexes unlist function will give to breakpoint2
-      //val break2 =  BreakPoint(breakpoints(3), breakpoints(4), breakpoints(5))
-
-      Some(SegmentedRegressionResult(List(break1), residuals, coefficients))
-
-    } catch {
-      case re: RuntimeException => Console.println("Exception: " + re.getMessage)
-        Option.empty
-    }
   }
 
   def linearRegression(temperature: Array[Double], energy: Array[Double]): Array[Double] = {
@@ -136,37 +164,7 @@ object WeatherNormalization {
 
     output.map { o =>
 
-      //    m1 = r("coefs['x']")[0]
-      //    m2 = r("coefs['U1.x']")[0]	+ m1
-      //    b = r("coefs['(Intercept)']")[0]
-      //
-      //    break1 = r("seg$psi[,'Est.']")[0]
-      //
-      //    rez = r("seg$residuals")
-      //    rez = sum(list(map(abs,rez)))
-      //
-      //    fitted_values = r("seg$fitted.values")
-      //    #print fitted_values
-      //
-      //    if (num_change == 1):
-      //
-      //    m3 = None
-      //    break2 = None
-      //
-      //    p_break1 = [break1, b + m1*break1]
-      //    p_0 = [min(x), p_break1[1] - m1*(break1-min(x))]
-      //    p_max = [max(x), m2*(max(x)-break1) +p_break1[1]]
-      //
-      //    #for plotting the regression
-      //    x_p = [p_0[0],p_break1[0], p_max[0]]
-      //    y_p = [p_0[1],p_break1[1], p_max[1]]
-      //
-      //    good_fit = rParamCheck.check(b,m1,m2,break1,num_change)
-      //
-      //    if good_fit == True:
-      //      y_hat = "y_hat (when Temperature <= " + str(break1) + ") = " + str(b) + " + " + str(m1) + "*Temperature\n" \
-      //      + "y_hat (when Temperature > " + str(break1) + ") = " + str(p_break1[1]) + " + " + str(m2) + "*(Temperature - " + str(break1) + ")"
-      //    CP_ref = "Change Point References @ " + "Start=" + str(p_0) + "Break_1=" + str(p_break1) + "End=" +str(p_max)
+      require(o.psi.length < 3, "No more than 2 break points supported")
 
       val m1: Double = o.coefficients(0)
       val m2: Double = o.coefficients(1) + m1
@@ -177,21 +175,101 @@ object WeatherNormalization {
       val re: Array[Double] = o.residuals
       val rez: Double =  re.map {r => r.abs}.sum
 
-      val m3 = Option.empty
-      val break2 = Option.empty
-      val p_break1 = List(break1, b + m1 * break1)
-      val p_0 = List(temperature.min, p_break1(1) - m1*(break1 - temperature.min))
-      val p_max = List(temperature.max, m2*(temperature.max - break1) + p_break1(1))
+      if (o.psi.length == 1) {
 
-      val x_p = List(p_0(0), p_break1(0), p_max(0))
-      val y_p = List(p_0(1), p_break1(1), p_max(1))
+        //    m1 = r("coefs['x']")[0]
+        //    m2 = r("coefs['U1.x']")[0]	+ m1
+        //    b = r("coefs['(Intercept)']")[0]
+        //
+        //    break1 = r("seg$psi[,'Est.']")[0]
+        //
+        //    rez = r("seg$residuals")
+        //    rez = sum(list(map(abs,rez)))
+        //
+        //    fitted_values = r("seg$fitted.values")
+        //    #print fitted_values
+        //
+        //    if (num_change == 1):
+        //
+        //    m3 = None
+        //    break2 = None
+        //
+        //    p_break1 = [break1, b + m1*break1]
+        //    p_0 = [min(x), p_break1[1] - m1*(break1-min(x))]
+        //    p_max = [max(x), m2*(max(x)-break1) +p_break1[1]]
+        //
+        //    #for plotting the regression
+        //    x_p = [p_0[0],p_break1[0], p_max[0]]
+        //    y_p = [p_0[1],p_break1[1], p_max[1]]
+        //
+        //    good_fit = rParamCheck.check(b,m1,m2,break1,num_change)
+        //
+        //    if good_fit == True:
+        //      y_hat = "y_hat (when Temperature <= " + str(break1) + ") = " + str(b) + " + " + str(m1) + "*Temperature\n" \
+        //      + "y_hat (when Temperature > " + str(break1) + ") = " + str(p_break1[1]) + " + " + str(m2) + "*(Temperature - " + str(break1) + ")"
+        //    CP_ref = "Change Point References @ " + "Start=" + str(p_0) + "Break_1=" + str(p_break1) + "End=" +str(p_max)
 
-      val y_hat = "y_hat (when Temperature <= " + break1 + ") = " + b + " + " + m1 + "*Temperature\n" +
-            "y_hat (when Temperature > " + break1 + ") = " + p_break1(1) + " + " + m2 + "*(Temperature - " + break1 + ")"
-      val CP_ref = "Change Point References @ " + "Start=" + p_0 + "Break_1=" + p_break1 + "End=" + p_max
+        val m3 = Option.empty
+        val break2 = Option.empty
+        val p_break1 = List(break1, b + m1 * break1)
+        val p_0 = List(temperature.min, p_break1(1) - m1*(break1 - temperature.min))
+        val p_max = List(temperature.max, m2*(temperature.max - break1) + p_break1(1))
 
-      Console.println(y_hat)
-      Console.println(CP_ref)
+        val x_p = List(p_0(0), p_break1(0), p_max(0))
+        val y_p = List(p_0(1), p_break1(1), p_max(1))
+
+        val y_hat = "y_hat (when Temperature <= " + break1 + ") = " + b + " + " + m1 + "*Temperature\n" +
+          "y_hat (when Temperature > " + break1 + ") = " + p_break1(1) + " + " + m2 + "*(Temperature - " + break1 + ")"
+        val CP_ref = "Change Point References @ " + "Start=" + p_0 + "Break_1=" + p_break1 + "End=" + p_max
+
+        Console.println(y_hat)
+        Console.println(CP_ref)
+
+      } else if(o.psi.length == 2) {
+
+//        break2 = r("seg$psi[,'Est.']")[1]
+//        m3 = r("coefs['U2.x']")[0]	+ m2
+//
+//        p_break1 = [break1, b + m1*break1]
+//        p_break2 = [break2, p_break1[1] + m2*(break2-break1)]
+//        p_0 = [min(x), p_break1[1] - m1*(break1-min(x))]
+//        p_max = [max(x), p_break2[1] + m3*(max(x)-break2)]
+//
+//
+//        #for plotting the regression
+//        x_p = [p_0[0],p_break1[0],p_break2[0], p_max[0]]
+//        y_p = [p_0[1],p_break1[1], p_break2[0], p_max[1]]
+//
+//        good_fit = rParamCheck.check(b,m1,m2,break1,num_change,m3 = m3,break2 = break2)
+//
+//        if good_fit == True:
+//          y_hat = "y_hat (when Temperature <= " + str(break1) + ") = " + str(b) + " + " + str(m1) + "*Temperature\n" \
+//          + "y_hat (when " +  str(break1) +  "< Temperature < " + str(break2) + ") = " + str(p_break1[1]) + " + " + str(m2) + "*(Temperature - " + str(break1) + ")" \
+//          + "y_hat (when Temperature > " + str(break2) + ") = " + str(p_break2[1]) + " + " + str(m3) + "*(Temperature - " + str(break2) + ")"
+//        CP_ref = "Change Point References @ " + "Start=" + str(p_0) + "Break_1=" + str(p_break1) + "Break2=" + str(p_break2) + "End=" +str(p_max)
+
+        val break2 = o.psi(1).est
+        val m3 = o.coefficients(3) + m2
+
+        val p_break1 = List(break1, b + m1 * break1)
+        val p_break2 = List(break2, p_break1(1) + m2 *(break2-break1))
+        val p_0 = List(temperature.min, p_break1(1) - m1 * (break1 - temperature.min))
+        val p_max = List(temperature.max, p_break2(1) + m3 * (temperature.max - break2))
+
+        val x_p = List(p_0(0), p_break1(0), p_break2(0), p_max(0))
+        val y_p = List(p_0(1), p_break1(1), p_break2(0), p_max(1))
+
+        val y_hat = "y_hat (when Temperature <= " + break1 + ") = " + b + " + " + m1 + "*Temperature\n" +
+                  "y_hat (when " +  break1 +  "< Temperature < " + break2 + ") = " + p_break1(1) + " + " + m2 + "*(Temperature - " + break1 + ")" +
+                  "y_hat (when Temperature > " + break2 + ") = " + p_break2(1) + " + " + m3 + "*(Temperature - " + break2 + ")"
+        val CP_ref = "Change Point References @ " + "Start=" + p_0 + "Break_1=" + p_break1 + "Break2=" + p_break2 + "End=" + p_max
+
+        Console.println(y_hat)
+        Console.println(CP_ref)
+
+      }
+
+
     }
 
   }
--- src/test/scala/TestWeatherNormalization.scala
+++ src/test/scala/TestWeatherNormalization.scala
@@ -56,6 +56,109 @@ class TestWeatherNormalization extends FunSuite with Matchers {
     51176,
     47931)
 
+  val twoBreakPoints = List(
+    (1,1.7239191),
+    (2,4.2878656),
+    (3,3.27346038),
+    (4,2.66136032),
+    (5,-1.14927723),
+    (6,5.43659812),
+    (7,8.65423904),
+    (8,4.91615703),
+    (9,0.69629976),
+    (10,0.02794051),
+    (11,6.32820731),
+    (12,5.26899847),
+    (13,10.72214919),
+    (14,7.58576613),
+    (15,4.46930104),
+    (16,6.21018625),
+    (17,0.18709438),
+    (18,6.7731688),
+    (19,-0.86813205),
+    (20,6.2294853),
+    (21,7.85343624),
+    (22,1.4705015),
+    (23,2.20744778),
+    (24,1.01744464),
+    (25,0.04188748),
+    (26,6.64326685),
+    (27,-3.53320882),
+    (28,5.20247412),
+    (29,10.15547056),
+    (30,2.77228798),
+    (31,5.48406741),
+    (32,5.74890377),
+    (33,5.98693697),
+    (34,1.33577526),
+    (35,4.02821054),
+    (36,7.90823629),
+    (37,-0.70376173),
+    (38,8.96453582),
+    (39,12.87977652),
+    (40,9.00326366),
+    (41,10.3964663),
+    (42,14.48171111),
+    (43,13.74171532),
+    (44,19.33860527),
+    (45,17.16899707),
+    (46,20.70187621),
+    (47,20.85317312),
+    (48,21.06441003),
+    (49,27.70659371),
+    (50,27.57895917),
+    (51,25.26440881),
+    (52,28.69244133),
+    (53,30.7489612),
+    (54,30.79223138),
+    (55,31.75451881),
+    (56,38.82941154),
+    (57,32.13396637),
+    (58,36.6687221),
+    (59,37.04588465),
+    (60,41.49368898),
+    (61,38.68815784),
+    (62,42.75642698),
+    (63,42.39840343),
+    (64,42.99214444),
+    (65,48.09226953),
+    (66,48.63483939),
+    (67,48.49687544),
+    (68,50.41330169),
+    (69,52.4110024),
+    (70,56.75985412),
+    (71,56.61008613),
+    (72,55.72858939),
+    (73,53.77154723),
+    (74,59.65604798),
+    (75,51.79574223),
+    (76,58.05488645),
+    (77,52.15339319),
+    (78,53.49587852),
+    (79,56.77761065),
+    (80,56.42431531),
+    (81,56.7140531),
+    (82,54.64947111),
+    (83,57.85813668),
+    (84,56.54413091),
+    (85,56.99742811),
+    (86,53.63121477),
+    (87,56.52490289),
+    (88,56.60286959),
+    (89,50.75918551),
+    (90,54.72055407),
+    (91,53.79721513),
+    (92,56.0811964),
+    (93,55.67949141),
+    (94,56.55870633),
+    (95,52.91268638),
+    (96,50.17357767),
+    (97,54.63292298),
+    (98,55.996081),
+    (99,60.8370713),
+    (100,54.34227929)
+  )
+
   test("test with 24 data points") {
 
     val result = WeatherNormalization.segmentedRegression(temperature, energy)
@@ -81,7 +184,7 @@ class TestWeatherNormalization extends FunSuite with Matchers {
       108784)
 
     val result = WeatherNormalization.segmentedRegression(temp, energy)
-//    result(0)(1) shouldBe  68.25 +- 0.2
+    result.map { r => r.psi(0).est shouldBe  68.25 +- 0.2 }
 
   }
 
@@ -100,6 +203,27 @@ class TestWeatherNormalization extends FunSuite with Matchers {
 
   }
 
+  test("test with 2 break points") {
+
+    val temperature = twoBreakPoints.map (pair => pair._1.toDouble).toArray
+    val energy = twoBreakPoints.map (pair => pair._2).toArray
+
+    val result = WeatherNormalization.segmentedRegression(temperature, energy)
+    result.map{r => r.psi(0).est shouldBe 35.0 +- 0.5}
+    result.map{r => r.psi(1).est shouldBe 70.0 +- 1.0}
+  }
+
+
+  test("calculate output with 24 data points and 1 break point") {
+    val result = WeatherNormalization.calculateOutput(temperature, energy)
+  }
+
+  test("calculate output with 100 data points and 2 break points") {
+    val temperature = twoBreakPoints.map (pair => pair._1.toDouble).toArray
+    val energy = twoBreakPoints.map (pair => pair._2).toArray
+    WeatherNormalization.calculateOutput(temperature, energy)
+  }
+
   test("Polyval test") {
 
     // input data taken from https://www.wired.com/2011/01/linear-regression-with-pylab/
@@ -112,10 +236,6 @@ class TestWeatherNormalization extends FunSuite with Matchers {
     val result = WeatherNormalization.polyval(temp, energy)
     result.length should equal(temp.length)
 
-  }
-
-  test("calculate output with 24 data points") {
-    WeatherNormalization.calculateOutput(temperature, energy)
   }
 
 }
